61426f2b46193d4bfa1c9334e01cd78532eaec82
Sebastian Czech <sebaczech@gmail.com>
2020-08-22 21:24:41 +0200

    init: Project created using Spring initializer

---
+++ src/test/java/com/sebastian/czech/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.czech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void should_context_load() {
+	}
+
+}
